ae0f28d41fcbf15923631fe49d09377e838763ef
AdrienGaillard <adrien.gaillard.pro@gmail.com>
2022-11-18 18:12:07 +0100

    Miising file from previous commit

--- word_comparator.py
+++ word_comparator.py
@@ -11,15 +11,19 @@ def word_comparator(group1: str, group2: str):
     """Returns the similarity score of the two input words
        Initially made for words but seems to work on noun groups"""
 
+    if group1 == group2:
+        return 1
+   
     # Lower words to avert case related problems
     token1 = nlp(group1.lower())
-    token2 = nlp(group2.lower())
+    if not token1.has_vector:
+        # print (token1.text, "Pas de vecteur")
+        return 0
 
-    # Attributes of each token :
-    # text: the word string
-    # has_vector: if it contains a vector representation in the model
-    # vector_norm: the algebraic norm of the vector
-    # is_oov: if the word is out of vocabulary
+    token2 = nlp(group2.lower())
+    if not token2.has_vector:
+        # print (token2.text, "Pas de vecteur")
+        return 0
 
     # TODO add a threshold to avert false similarity
     return token1.similarity(token2)
