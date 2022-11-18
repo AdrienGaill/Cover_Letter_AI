1219b1f845cf9591daf472409a7274d655fc1c4a
AdrienGaillard <adrien.gaillard.pro@gmail.com>
2022-11-18 18:08:23 +0100

    Turned pdf_creator in a function and added it to main file

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from word_comparator import word_comparator
 from offer_extractor import offer_extractor
+from pdf_creator import letter_generator
 
-"""Intent to create an AI powered cover letter writting algorithm"""
+"""Main file to create an AI powered cover letter writting algorithm"""
 
+# Extract reference words from .txt file
 f = open("./word_list.txt", "r")
 ref_words = [x.strip() for x in f.readlines()]
 f.close()
 
+# Extract keyphrases from offer
 url = "https://www.linkedin.com/jobs/view/3341568256/?alternateChannel=search&refId=QYh%2BjS4URfirSywQlvBYEQ%3D%3D&trackingId=OqKmUh10OQq6J1YAhsRL9g%3D%3D"
 data = offer_extractor(url)
 keyphrases = data["keyphrases"]
-sorted_kp = [ [x, []] for x in ref_words]
 
 # Sort keyphrases extracted from offer using word comparison and a list of reference words
+sorted_kp = [ [x, []] for x in ref_words]
 for kp in keyphrases:
     max_score = 0
     best_index = -1
@@ -26,4 +29,11 @@ for kp in keyphrases:
         sorted_kp[best_index][1].append(kp)
 
 # TODO choose sentence allocation given the sorted kw
+paragraphs = ["Dear Sir or Madam",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    ]
 
+# Generate the pdf file with the paraphrased paragraphs
+letter_generator(paragraphs)
--- pdf_creator.py
+++ pdf_creator.py
@@ -4,55 +4,53 @@ from datetime import date, datetime
 from reportlab.platypus import Paragraph, SimpleDocTemplate
 from reportlab.lib.styles import ParagraphStyle
 
-fileName = 'Cover Letter Adrien Gaillard.pdf'
-elements = []
-# creating a pdf object
-pdf = SimpleDocTemplate(fileName)
 
-# Registering external fonts
-pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))
-pdfmetrics.registerFont(TTFont("Arial bold", 'Arial_bold.ttf'))
+def letter_generator(paragraphs: list):
+    """ Generate the cover letter in a pdf file """
 
+    fileName = 'Cover Letter Adrien Gaillard.pdf'
 
-# Add title
-title = "Adrien GAILLARD"
-title_style = ParagraphStyle(name = "", fontSize = 14, fontName = "Arial bold", leading=14)
-title_para = Paragraph(title, style=title_style)
-elements.append(title_para)
+    # Create the template object
+    pdf = SimpleDocTemplate(fileName)
 
+    # Elements to be rendered in the pdf file
+    elements = []
 
-# Add references
-final_date = datetime.strftime(date.today(), "%B %d, %Y")
-subtitle = f'''+33 631 651 529<br/>
-    <a href=http://google.com>adrien.gaillard.pro@gmail.com</a><br/>
-    My <a color="blue" href=https://www.linkedin.com/in/AdrienGaillard/>LinkedIn</a><br/>
-    <br/>
-    {final_date}<br/><br/><br/>
+    # Registering external fonts
+    pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))
+    pdfmetrics.registerFont(TTFont("Arial bold", 'Arial_bold.ttf'))
+
+    # Add title
+    title = "Adrien GAILLARD"
+    title_style = ParagraphStyle(name = "", fontSize = 14, fontName = "Arial bold", leading=14)
+    title_para = Paragraph(title, style=title_style)
+    elements.append(title_para)
+
+    # Add references and date
+    final_date = datetime.strftime(date.today(), "%B %d, %Y")
+    subtitle = f'''+33 631 651 529<br/>
+        <a href=http://google.com>adrien.gaillard.pro@gmail.com</a><br/>
+        My <a color="blue" href=https://www.linkedin.com/in/AdrienGaillard/>LinkedIn</a><br/>
+        <br/>
+        {final_date}<br/><br/><br/>
+        '''
+    main_style = ParagraphStyle(name = "", fontSize = 11, fontName = "Arial")
+    elements.append(Paragraph(subtitle, style=main_style))
+
+    # Add paraphrased paragraphs
+    for paragraph in paragraphs:
+        elements.append(Paragraph(paragraph+"<br/><br/>", style=main_style))
+
+    # Add signature at the end
+    image = '''
+        Adrien Gaillard<br/>
+        <img valign='text-top' src=./signature_adrien.png width='80' height='60'/>
     '''
-main_style = ParagraphStyle(name = "", fontSize = 11, fontName = "Arial")
-elements.append(Paragraph(subtitle, style=main_style))
+    std_indented = ParagraphStyle(name="std_indented", fontName="Arial", fontSize=11, leftIndent=44, )
+    signature = Paragraph(image, style=std_indented)
+    elements.append(signature)
 
+    # Build every element and save the pdf
+    pdf.build(elements)
 
-# Add paraphrased paragraphs
-# TODO get paragraphs
-paragraphs = ["Dear Sir or Madam",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    ]
-for paragraph in paragraphs:
-    elements.append(Paragraph(paragraph+"<br/><br/>", style=main_style))
-
-
-# Add signature at the end
-image = '''
-    Adrien Gaillard<br/>
-    <img valign='text-top' src=./signature_adrien.png width='80' height='60'/>
-'''
-std_indented = ParagraphStyle(name="std_indented", fontName="Arial", fontSize=11, leftIndent=44, )
-signature = Paragraph(image, style=std_indented)
-elements.append(signature)
-
-
-# Build every element and save the pdf
-pdf.build(elements)
+# letter_generator(["Rien"])
