49265e2287833d854ca436a26f10fb67604f084e
AdrienGaillard <adrien.gaillard.pro@gmail.com>
2022-11-18 18:10:29 +0100

    Cleaned similar_sentences and fixed word without vector representation in word_comparator

--- similar_sentences.py
+++ similar_sentences.py
@@ -14,17 +14,22 @@ parrot = Parrot(model_tag="parrot_paraphraser_on_T5", use_gpu=False)
 test = "Having followed an education with a strong emphasis on computer science, including many courses in humanities and business, I am naturally versatile and curious and know how to integrate efficiently and flexibly into a team. I speak fluent English (TOEIC 980) and I am currently learning German. Therefore I would like to propose that we meet in order to convince you of my motivation and I am already enclosing my CV to that effect. I look forward to your reply and thank you in advance for your time and attention. I remain available for any information."
 
 def text_slicer(text: str):
-  # TODO handle . at the end of sentences and line break
+  """Split a paragraph after each dot in a list of sentences"""
   return text.split(". ")
 
 
 # TODO Fix the case of special words 
 def text_finisher(sentences: list):
+  """Format a list of strings in one paragraph : capitalize each sentence,
+  replace the i by upper-case ones and each proper noun by its correct spelling"""
   result = ""
   for sentence in sentences:
     sentence = sentence.capitalize()
     result += sentence + ". "
   result.replace(" i ", " I ")
+  result.replace("react", "React")
+  result.replace("python", "Python")
+
   return result
 
 
