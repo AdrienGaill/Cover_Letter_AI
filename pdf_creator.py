ca2191cbc4a5875f369ac8768191efd747bb601d
AdrienGaillard <adrien.gaillard.pro@gmail.com>
2023-01-05 18:04:44 +0100

    Improved and cleaned the pdf_creator function

--- pdf_creator.py
+++ pdf_creator.py
@@ -5,7 +5,7 @@ from reportlab.platypus import Paragraph, SimpleDocTemplate
 from reportlab.lib.styles import ParagraphStyle
 
 
-def get_cover_letter(paragraphs: list):
+def get_cover_letter(paragraphs: list, offer_title: str):
     """ Generate the cover letter in a pdf file """
 
     fileName = 'Cover Letter Adrien Gaillard.pdf'
@@ -16,34 +16,58 @@ def get_cover_letter(paragraphs: list):
     # Elements to be rendered in the pdf file
     elements = []
 
-    # Registering external fonts
+    # Register fonts as a family
     pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))
-    pdfmetrics.registerFont(TTFont("Arial bold", 'Arial_bold.ttf'))
+    pdfmetrics.registerFont(TTFont("Arial-Bold", 'Arial_bold.ttf'))
+    pdfmetrics.registerFont(TTFont("Arial-Italic", 'Arial_Italic.ttf'))
+    pdfmetrics.registerFont(TTFont("Arial-Bold_Italic", 'Arial_Bold_Italic.ttf'))
+    pdfmetrics.registerFontFamily('Arial', normal='Arial',
+                                      bold='Arial-Bold',
+                                      italic='Arial-Italic',
+                                      boldItalic='Arial-Bold-Italic')
 
     # Add title
-    title = "Adrien GAILLARD"
-    title_style = ParagraphStyle(name = "", fontSize = 14, fontName = "Arial bold", leading=14)
-    title_para = Paragraph(title, style=title_style)
+    title_style = ParagraphStyle(name = "", fontSize = 14, fontName = "Arial", leading = 16)
+    subtitle_style = ParagraphStyle(name = "", fontSize = 11, fontName = "Arial", leading = 14, alignment = 4)
+    main_style = ParagraphStyle(name = "", fontSize = 11, fontName = "Arial", leading=14, firstLineIndent = 22, alignment = 4)
+
+    title_para = Paragraph("<b>Adrien GAILLARD</b>", style=title_style)
     elements.append(title_para)
 
-    #TODO Add GitHub link
     # Add references and date
-    final_date = datetime.strftime(date.today(), "%B %d, %Y")
+    today = date.today()
+    day = today.day
+    month = datetime.strftime(today, "%B")
+    year = datetime.strftime(today, ", %Y")
     subtitle = f'''
         +33 631 651 529<br/>
-        <a href=http://google.com>adrien.gaillard.pro@gmail.com</a><br/>
+        <a href=mailto:adrien.gaillard.pro@gmail.com>adrien.gaillard.pro@gmail.com</a><br/>
         My <a color="blue" href=https://www.linkedin.com/in/AdrienGaillard/>LinkedIn</a><br/>
         My <a color="blue" href=https://github.com/AdrienGaill/>GitHub</a><br/>
         <br/>
-        {final_date}<br/><br/><br/>
+        {month} {day}{year}<br/><br/><br/>
         '''
-    main_style = ParagraphStyle(name = "", fontSize = 11, fontName = "Arial")
-    elements.append(Paragraph(subtitle, style=main_style))
+    elements.append(Paragraph(subtitle, style=subtitle_style))
+
+    elements.append(Paragraph("Dear Sir or Madam,<br/><br/>", style = main_style))
+
+    AI_project = "First of all, I didn't write this cover letter. Indeed, it was created by an algorithm using NLP to reflect your job offer's requirements. You can learn more about <a color='blue' href=https://github.com/AdrienGaill/Cover_Letter_AI/>this project</a> on my GitHub.<br/><br/>"
+    elements.append(Paragraph(AI_project, style=main_style))
+
+    internship = f'''Currently on a gap year after my 2nd year at Ecole Centrale de Nantes, a top French engineering school, I am seeking an internship or a fixed-term contract of at least 6 months in an IT related field, preferably AI, located in Germany, in Berlin.
+        Thus I'm interested in the <b>{offer_title}</b> position you offer.<br/><br/>
+        '''
+        
+    elements.append(Paragraph(internship, style=main_style))
 
     # Add paraphrased paragraphs
     for paragraph in paragraphs:
         elements.append(Paragraph(paragraph+"<br/><br/>", style=main_style))
 
+    elements.append(Paragraph("Therefore I would like to propose that we meet in order to convince you of my motivation and I am already enclosing my CV to that effect. I look forward to your reply and thank you in advance for your time and attention.<br/><br/>", style=main_style))
+
+    elements.append(Paragraph("Sincerely,<br/><br/>", style=main_style))
+    
     # Add signature at the end
     image = '''
         Adrien Gaillard<br/>
@@ -56,4 +80,3 @@ def get_cover_letter(paragraphs: list):
     # Build every element and save the pdf
     pdf.build(elements)
 
-# letter_generator(["Rien"])
